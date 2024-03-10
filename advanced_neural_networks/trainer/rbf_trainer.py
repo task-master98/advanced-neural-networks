e0944399fa218758b404d79ed2ff389ecc0ced76
Ishaan Roy <ishaanroy@Ishaans-MacBook-Air.local>
2024-03-10 01:14:36 -0700

    Added K-means clustering to train methods

--- advanced_neural_networks/trainer/rbf_trainer.py
+++ advanced_neural_networks/trainer/rbf_trainer.py
@@ -12,14 +12,14 @@ import pandas as pd
 from sklearn.cluster import KMeans
 import torch
 from torch.utils.data import Subset, DataLoader
+import torch.nn.functional as F
 import yaml
 from tqdm import tqdm, tqdm_notebook
 
 import advanced_neural_networks
 from advanced_neural_networks.trainer.trainer import MNISTTrainer
 from advanced_neural_networks.dataloader.representation_dataset import Representations
-from advanced_neural_networks.models.rbf_layer import RBFLayer
-from advanced_neural_networks.models.rbf_utils import *
+from advanced_neural_networks.models.rbf_layer import RBFNetwork
 
 class RBFTrainer(MNISTTrainer):
 
@@ -28,7 +28,8 @@ class RBFTrainer(MNISTTrainer):
                       "rmsprop": torch.optim.RMSprop}
     
     LOSS_DICT = {"bce": torch.nn.BCEWithLogitsLoss,
-                 "cross_entropy": torch.nn.CrossEntropyLoss}
+                 "cross_entropy": torch.nn.CrossEntropyLoss,
+                 "mse": torch.nn.MSELoss}
 
     DATA_PREFIX = {"MNIST": "mnist",
                    "FashionMNIST": "fashion_mnist",
@@ -58,58 +59,73 @@ class RBFTrainer(MNISTTrainer):
         self.learning_rate = self.trainer_config["lr"]
         self.batch_size = self.trainer_config["batch_size"]
         self.model = None
+        self.num_classes = 10
+        self.x_train_numpy = self.convert_dataset_to_numpy()
     
-    # def find_cluster_centers(self, feature_vec, n_clusters):        
-    #     k_means = KMeans(n_clusters = n_clusters)
-    #     k_means.fit(feature_vec)
-    #     cluster_centers = torch.Tensor(k_means.cluster_centers_)
-    #     return cluster_centers 
+    def find_cluster_centers(self, feature_vec, n_clusters, max_iter = 100):        
+        k_means = KMeans(n_clusters = n_clusters, max_iter = max_iter)
+        k_means.fit(feature_vec)
+        cluster_centers = torch.Tensor(k_means.cluster_centers_, dtype=torch.float32)
+        return cluster_centers 
 
     def train_epoch(self, iterator, model, device):
         epoch_loss = 0.0
-        epoch_acc = 0.0        
+        correct_predictions = 0
+        total_samples = 0       
         model.train()
 
         for (x, y) in tqdm_notebook(iterator, desc="Training", leave=False):
 
             x = x.to(device)
-            y = y.squeeze().to(device)
-            
-            self.optimizer.zero_grad()
-
             y_pred = model(x)
+            y_onehot = F.one_hot(y.long(), num_classes = self.num_classes).squeeze()
+            y_onehot = y_onehot.to(device)            
+            loss = self.criterion(y_pred, y_onehot.float()) 
 
-            loss = self.criterion(y_pred, y.long())
-            acc = self.calculate_accuracy(y_pred, y.long())
-
+            self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
 
-            epoch_loss += loss.item()
-            epoch_acc += acc
+            epoch_loss += loss.item() * x.size(0)
+            _, predicted = torch.max(y_pred, 1)
+            correct_predictions += (predicted == torch.argmax(y_onehot, 1)).sum().item()
+            total_samples += y_onehot.size(0) 
         
-        return epoch_loss / len(iterator), epoch_acc / len(iterator)
+        return epoch_loss / total_samples, correct_predictions / total_samples
     
     def evaluate_epoch(self, iterator, model, device):
 
         epoch_loss = 0.0
-        epoch_acc = 0.0        
+        correct_predictions = 0
+        total_samples = 0       
         model.eval()
 
         with torch.no_grad():            
             for (x, y) in tqdm_notebook(iterator, desc="Evaluating", leave=False):
 
                 x = x.to(device)
-                y = y.squeeze().to(device)
-
                 y_pred = model(x)
-                loss = self.criterion(y_pred, y.long())
-                acc = self.calculate_accuracy(y_pred, y.long())
+                y_onehot = F.one_hot(y.long(), num_classes = self.num_classes).squeeze()
+                y_onehot = y_onehot.to(device)            
+                loss = self.criterion(y_pred, y_onehot.float())
 
-                epoch_loss += loss.item()
-                epoch_acc += acc
+                epoch_loss += loss.item() * x.size(0)
+                _, predicted = torch.max(y_pred, 1)
+                correct_predictions += (predicted == torch.argmax(y_onehot, 1)).sum().item()
+                total_samples += y_onehot.size(0)
+                
 
-        return epoch_loss / len(iterator), epoch_acc / len(iterator)
+        return epoch_loss / total_samples, correct_predictions / total_samples
+    
+    def convert_dataset_to_numpy(self):
+        x_train = []
+        for itr in tqdm_notebook(range(len(self.dataset))):
+            x, _ = self.dataset[itr]
+            x_train.append(x.numpy())
+
+        x_train = np.array(x_train)
+        return x_train
+
 
     def train(self, rbf_params, optimizer_params):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
@@ -121,7 +137,15 @@ class RBFTrainer(MNISTTrainer):
         val_iterator = DataLoader(self.val_dataset,
                                   batch_size = self.batch_size) 
 
-        rbf_layer = RBFLayer(**rbf_params)
+        n_clusters = rbf_params["kmeans"]["n_clusters"]
+        max_iter = rbf_params["kmeans"]["max_iter"]
+        print(f"Finding Cluster Centers...")
+        cluster_centers = self.find_cluster_centers(self.x_train_numpy, n_clusters, max_iter)
+
+        model_params = rbf_params["model"]
+        model_params["centers"] = cluster_centers
+
+        rbf_layer = RBFNetwork(**model_params)
         rbf_layer = rbf_layer.to(device)
         self.configure_optimizers(rbf_layer, **optimizer_params)
         self.criterion = self.criterion.to(device)        
